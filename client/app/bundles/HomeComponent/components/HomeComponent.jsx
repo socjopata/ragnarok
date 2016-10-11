7ff37b2f38dae5e8b5c667ad79364935989ebd47
dsznajder <damsznajder@gmail.com>
2016-10-11 09:15:30 +0200

    removed unnecessary comments

--- client/app/bundles/HomeComponent/components/HomeComponent.jsx
+++ client/app/bundles/HomeComponent/components/HomeComponent.jsx
@@ -1,9 +1,5 @@
-// HomeComponentWidget is an arbitrary name for any "dumb" component. We do not recommend suffixing
-// all your dump component names with Widget.
-
 import React, { PropTypes } from 'react';
 
-// Simple example of a React "dumb" component
 export default class HomeComponent extends React.Component {
   render() {
     return (
