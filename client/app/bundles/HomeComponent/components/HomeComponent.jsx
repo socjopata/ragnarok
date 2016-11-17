ff0752932728eddd83ee89b913ec7062bbf41db0
socjopata <socjopata@gmail.com>
2016-11-17 22:22:53 +0100

    Improve the formatting

--- client/app/bundles/HomeComponent/components/HomeComponent.jsx
+++ client/app/bundles/HomeComponent/components/HomeComponent.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
 export default class HomeComponent extends React.Component {
-    render() {
-        return (
-            <div>
-            <h3>
-            Kreator postaci Ragnarok
+  render() {
+    return (
+      <div>
+        <h3>
+          Kreator postaci Ragnarok
         </h3>
-        </div>
+      </div>
     );
-    }
+  }
 }
